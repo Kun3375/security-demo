cdda1c03bd951ce84af90a2dac893aec9e35071f
= <=>
2017-11-16 19:57:04 +0800

    11.16.19.57

--- security-browser/src/main/java/com/kun/security/browser/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com/kun/security/browser/BrowserSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.kun.security.browser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author CaoZiye
@@ -10,6 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
---
+++ security-browser/src/main/java/com/kun/security/browser/MyUserDetailService.java
@@ -0,0 +1,29 @@
+package com.kun.security.browser;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author CaoZiye
+ * @version 1.0 2017/11/16 19:37
+ */
+@Component
+public class MyUserDetailService implements UserDetailsService{
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 查询数据库
+        return new User(
+                "kun", // 用户名
+                "$2a$10$COHPGeNDbIC/QZGIH3339.N8S391mZaj5B5/MyfhilHQ4GnEmcTh6", // 加密密码
+                true, // 有效
+                true, // 账户未过期
+                true, // 密码未过期
+                true, // 账户未锁定
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin")); // 权限
+    }
+}
